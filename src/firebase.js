600ef34c1c98bd3a173d4bd399b5d0d0c08b3e6a
tomo-0119 <19s8001@ogu.ac.jp>
2022-03-05 20:25:22 +0900

    .prettierrcの追加

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAaGxVJawEjhbuqn4MZkg1pNo7R75VMo1c",
+  apiKey: "AIzaSyAaGxVJawEjhbuqn4MZkg1pNo7R75VMo1c",
   authDomain: "team-skiing.firebaseapp.com",
   projectId: "team-skiing",
   storageBucket: "team-skiing.appspot.com",
   messagingSenderId: "371368152028",
   appId: "1:371368152028:web:48edad0f922e2f7b63dd19",
-  measurementId: "G-QYNJL9P90W"
+  measurementId: "G-QYNJL9P90W",
 }
 
 // Firebaseアプリオブジェクトを初期化
