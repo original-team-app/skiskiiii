a61a5784d8d7aebf02a1cb543d15b66fbe11258c
tomo-0119 <19s8001@ogu.ac.jp>
2022-03-12 18:59:47 +0900

    api.vueを新規追加、API読み込みまで完了

---
+++ src/views/Api.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="api">
+    <h1>This is an API page</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+// APIの取得
+fetch("http://jws.jalan.net/APIAdvance/HotelSearch/V1/")
+//   .then((res) => {
+//     return res.json()
+//   })
+//   .then((json) => {
+//     imageElement.src = json.message
+//   })
+</script>
