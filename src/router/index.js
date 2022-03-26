e0fa790a68cbbf222d052cdb0de4a7da02270582
tomo-0119 <19s8001@ogu.ac.jp>
2022-03-26 10:10:23 +0900

    楽天API

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/api",
+    name: "Api",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Api.vue"),
+  },
 ]
 
 const router = createRouter({
